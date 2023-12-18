10549c71c2a9ce750ee904deff36022a7de0dac1
ilhamdika <93665288+ilhamdika@users.noreply.github.com>
2023-12-18 23:28:53 +0700

    feat: setup project

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className='bg-blue-500'>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1 className='text-black'>test</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
