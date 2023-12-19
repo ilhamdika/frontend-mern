860f4ef4cb447758f74339970e547a11f4be5c08
ilhamdika <93665288+ilhamdika@users.noreply.github.com>
2023-12-19 17:44:06 +0700

    feat: make button component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from './component/Button';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Button>test</Button>
       </header>
     </div>
   );
